8b5dcbc7f3605326cfd75b4980f4aea3e3edf4b7
HanamiGarduque <garduquehanami@gmail.com>
2024-10-31 10:17:38 +0800

    Added home page file

---
+++ BLib.php
@@ -0,0 +1,22 @@
+<?php
+$welcomeMessage = 'BLib:';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Library Website</title>
+
+        <link rel="stylesheet" href="HomePage.css">
+    </head>
+    <!-- Navigation Bar -->
+    <nav>
+        <ul>
+            <li><a href="BLib.php">Home</a></li>
+            <li><a href="search.php">Search Books</a></li>
+            <li><a href="about.php">About Us</a></li>
+            <li><a href="contact.php">Contact Us</a></li>
+        </ul>
+    </nav>

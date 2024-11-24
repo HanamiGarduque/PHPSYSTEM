2170065dc6c9996bd8e862b66ca3974a8caec77b
nami <garduquehanami@gmail.com>
2024-11-25 01:42:33 +0800

    added duration and status function and modified the input date conditions

--- reservationDB.php
+++ reservationDB.php
@@ -12,28 +12,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Initialize the Reservations object
     $reservation = new Reservations($db);
     $book = new Books($db);
+    $Book_ID = isset($_POST['book_id']) ? $_POST['book_id'] : die('ERROR: Book ID not found.');
+    $book->Book_ID = $Book_ID;  // Set the Book_ID propert
+
 
     // Sanitize and assign form data to the reservation object
-    $reservation->name = htmlspecialchars(trim($_POST['username']));
+    $reservation->book_id = $Book_ID;
+    $reservation->name = htmlspecialchars(trim($_POST['name']));
     $reservation->email = htmlspecialchars(trim($_POST['email']));
     $reservation->phone_number = htmlspecialchars(trim($_POST['phone_number']));
     $reservation->reservation_date = htmlspecialchars(trim($_POST['reservation_date']));
     $reservation->pickup_date = htmlspecialchars(trim($_POST['pickup_date']));
     $reservation->duration = htmlspecialchars(trim($_POST['duration']));
     $reservation->expected_return_date = htmlspecialchars(trim($_POST['expected_return_date']));    
-    $reservation->status = htmlspecialchars(trim($_POST['status']));
     $reservation->notes = htmlspecialchars(trim($_POST['notes']));
+
+
 
     // Attempt to create the reservation
     if ($reservation->create()) {
-        $books->updateBookCopies(); 
+        $book->updateBookCopies($Book_ID); 
         $_SESSION['status'] = 'success';
 
     } else {
         $_SESSION['status'] = 'error';
     }
 
-    header("Location: bookCatalog.php");
+    header("Location: book.php");
     exit();
         
     
